3e2583a714b5f15ce563cd04716bb2e2eebfd005
Courtney Stuart <courtney.e.stuart@gmail.com>
2026-09-14 14:59:34 +0100

    Conduct variance partitioning for updated occurrence model

--- Code/09A_Partition_Occurrence_Model_Variance.R
+++ Code/09A_Partition_Occurrence_Model_Variance.R
@@ -5,13 +5,13 @@
 # install required packages (first run only)
 # install.packages(c("easypackages", "conflicted", "tidyr", "dplyr", "here", "ggplot2",
 #                    "Hmsc", "coda", "corrplot", "Cairo", "stringr", "PNWColors",
-#                    "colorspace", "grDevices", "colorRamps", "tibble"))
+#                    "colorspace", "grDevices", "colorRamps", "tibble", "ape"))
 
 # load packages
 library(easypackages)
 libraries("conflicted", "tidyr", "dplyr", "here", "ggplot2",
           "Hmsc", "coda", "corrplot", "Cairo", "stringr", 
-          "PNWColors", "colorspace", "grDevices", "colorRamps", "tibble")
+          "PNWColors", "colorspace", "grDevices", "colorRamps", "tibble", "ape")
 
 # resolve package conflicts
 conflict_prefer("select", "dplyr")
@@ -40,11 +40,11 @@ list.files(model.directory)
 
 # read in the results file
 nChains = 4
-samples = 1000
-thin = 100
+samples = 4000
+thin = 50
 filename = file.path(model.directory, 
                      paste0("PA_model_chains_", as.character(nChains),
-                            "_samples_",as.character(samples),
+                            "_total_samples_",as.character(samples),
                             "_thin_",as.character(thin),".rda"))
 load(filename)
 
@@ -138,11 +138,13 @@ random_sp_half = FR_long %>%
 cols = c("blue4", "darkorange")  # blue = fixed, orange = random
 
 # set the file name, save location, and settings
-jpeg(filename = here("Figures", "PA_Model", "Variance_Partitioning_Fixed_vs_Random.jpg"),
+jpeg(filename = here("Figures", "PA_Model",
+                     "Variance_Partitioning_Fixed_vs_Random.jpg"),
      width = 14,
      height = 7,
      units = "in",
-     res = 450)
+     res = 450,
+     quality = 100)
 
 par(mar = c(5.5, 3.5, 0.0, 0.0) + 0.1,
     mgp = c(2.15, 0.5, 0),
@@ -277,7 +279,8 @@ jpeg(filename = here("Figures", "PA_Model", "Variance_Partitioning_Grouped.jpg")
      width = 16, 
      height = 8, 
      units = "in", 
-     res = 450)
+     res = 450,
+     quality = 100)
 
 par(mar = c(5.5, 3.5, 0.25, 9.25) + 0.1,   # increase bottom and right margins
     mgp = c(2.15, 0.5, 0),  # moves axis title and labels closer
@@ -300,7 +303,7 @@ text(x = bp,
      srt = 90,
      adj = 1, 
      xpd = TRUE,
-     cex = 0.65,
+     cex = 0.6,
      font = 3)
 
 # add legend outside plot area (to the top-right)
@@ -319,7 +322,8 @@ jpeg(
   width = 9,
   height = 13,
   units = "in",
-  res = 450)
+  res = 450,
+  quality = 100)
 
 par(
   mar = c(5.5, 5.5, 0, 0.5),
@@ -347,7 +351,7 @@ axis(
   labels = species_labels, #use colnames(vp_ungrouped_vals) if you want full names instead
   las = 2,
   font = 3,
-  cex.axis = 0.475)
+  cex.axis = 0.4)
 
 # # bottom legend with extra separation
 legend(
@@ -374,11 +378,26 @@ write.csv(vp_grouped_transposed,
           row.names = FALSE)
 
 #### UNGROUPED FIXED EFFECTS ####
-# variance partitioning without groupings 
-vp_ungrouped = computeVariancePartitioning(PA_model)
+# variance partitioning without grouping fixed-effect covariates
+# the intercept is assigned to the first group because it has no variation
+group = c(1, 1:(ncol(PA_model$X) - 1))
+groupnames = colnames(PA_model$X)[-1]
+
+vp_ungrouped = computeVariancePartitioning(
+  PA_model,
+  group = group,
+  groupnames = groupnames)
 
 # examine results
 vp_ungrouped$vals # variance proportion for each variable and species
+
+# check that the group vector has one value for every column of X
+length(group) # should be 16
+ncol(PA_model$X) # should be 16
+
+# check group assignments
+group
+groupnames
 
 # these will be the same as for vp_grouped above
 # vp_ungrouped$R2T
@@ -420,7 +439,8 @@ jpeg(filename = here("Figures", "PA_Model", "Variance_Partitioning_Not_Grouped.j
      width = 16, 
      height = 8, 
      units = "in", 
-     res = 450)
+     res = 450,
+     quality = 100)
 
 par(mar = c(5.5, 3, 0.25, 11.25) + 0.1,
     mgp = c(2, 0.5, 0),
@@ -443,7 +463,7 @@ text(x = bp,
      srt = 90,
      adj = 1,
      xpd = TRUE,
-     cex = 0.625,
+     cex = 0.6,
      font = 3)
 
 legend(x = par("usr")[2] - 6,
@@ -470,7 +490,8 @@ jpeg(
   width = 8,
   height = 14,
   units = "in",
-  res = 450)
+  res = 450,
+  quality = 100)
 
 par(
   mar = c(9.75, 5.5, 0, 0.5),
@@ -497,7 +518,7 @@ axis(
   labels = species_labels, #use colnames(vp_ungrouped_vals) if you want full names instead
   las = 2,
   font = 3,
-  cex.axis = 0.5)
+  cex.axis = 0.4)
 
 # # bottom legend with extra separation
 legend(
@@ -521,3 +542,515 @@ vp_ungrouped_transposed = vp_ungrouped_vals %>%
 write.csv(vp_ungrouped_transposed,
           here("HMSC", "Data", "Variance_Partitioning_Ungrouped.csv"),
           row.names = FALSE)
+
+#### GROUPED - PHYLOGENETIC ORDERING ####
+# extract the phylogenetic tree directly from the Hmsc model
+phylo_tree = PA_model$phyloTree
+
+# create a copy of the tree for plotting
+phylo_tree_plot = phylo_tree
+
+# create abbreviated species labels for plotting
+phylo_tree_plot$tip.label = sapply(
+  strsplit(phylo_tree$tip.label, "\\."),
+  function(x) paste0(
+    substr(x[1], 1, 1),
+    ". ",
+    paste(x[-1], collapse = " ")))
+
+# extract the phylogenetic tip order
+phylo_order = match(
+  phylo_tree$tip.label,
+  colnames(vp_grouped_vals))
+
+# check that all species are present in the variance partitioning results
+if (any(is.na(phylo_order))) {
+  stop("some species in the phylogenetic tree are missing from vp_grouped_vals.")
+}
+
+# reorder variance partitioning values according to phylogenetic tip order
+vp_grouped_vals_phylo = vp_grouped_vals[, phylo_order]
+
+# calculate mean variance explained by each component
+vp_grouped_means_phylo = rowMeans(vp_grouped_vals_phylo)
+
+# create legend labels with means
+legend_labels_phylo = paste0(
+  rownames(vp_grouped_vals_phylo),
+  " (mean = ",
+  round(vp_grouped_means_phylo, 2),
+  ")")
+
+##### identify species with poor beta convergence #####
+# species for which variance partitioning should not be interpreted
+problem_species = c(
+  "Neocirrhites.armatus",
+  "Zebrasoma.scopas",
+  "Centropyge.flavissima")
+
+# identify their positions in the phylogenetically ordered results
+problem_idx = phylo_tree$tip.label %in% problem_species
+
+# check that all specified species occur in the tree
+if (!all(problem_species %in% phylo_tree$tip.label)) {
+  stop("one or more problem_species are missing from the phylogenetic tree.")
+}
+
+##### determine how much of the phylogeny to show #####
+# calculate the depth of each node from the root
+node_depth = ape::node.depth.edgelength(phylo_tree)
+
+# find the maximum root-to-tip distance
+max_tree_depth = max(node_depth)
+
+# proportion of the tree to show
+recent_tree_fraction = 0.10
+
+# calculate the left-hand limit of the displayed tree
+tree_xmin = max_tree_depth - (
+  max_tree_depth * recent_tree_fraction)
+
+##### create portrait figure #####
+jpeg(
+  filename = here(
+    "Figures","PA_Model",
+    "Variance_Partitioning_Grouped_Portrait_Phylogenetic_Recent.jpg"),
+  width = 11,
+  height = 14,
+  units = "in",
+  res = 600,
+  quality = 100)
+
+##### colors #####
+vp_cols = colorRamps::matlab.like(
+  nrow(vp_grouped_vals_phylo))
+
+# color used to flag species with poor beta convergence
+poor_convergence_col = "grey40"
+
+##### main plot area #####
+# tree, labels and variance plot use exactly the same vertical coordinates
+main_bottom = 0.07
+main_top = 1
+
+##### phylogenetic tree #####
+par(
+  fig = c(0.00, 0.1, main_bottom, main_top),
+  mar = c(2.2, 0, 0, 0),
+  xpd = FALSE)
+
+# plot the terminal portion of the phylogeny without tip labels
+ape::plot.phylo(
+  phylo_tree_plot,
+  type = "phylogram",
+  direction = "rightwards",
+  show.tip.label = FALSE,
+  edge.width = 0.75,
+  x.lim = c(
+    tree_xmin,
+    max_tree_depth),
+  y.lim = c(
+    0.5,
+    length(phylo_tree$tip.label) + 0.5),
+  no.margin = FALSE)
+
+# retrieve the coordinates used by plot.phylo
+phylo_coords = get(
+  "last_plot.phylo",
+  envir = .PlotPhyloEnv)
+
+# extract the y-position of each species tip
+tip_y = phylo_coords$yy[
+  seq_len(length(phylo_tree$tip.label))]
+
+##### species labels #####
+par(
+  fig = c(0.1, 0.21, main_bottom, main_top),
+  mar = c(2.2, 0, 0, 0),
+  xpd = FALSE,
+  new = TRUE)
+
+# create empty plotting region for species labels
+plot(
+  NA,
+  xlim = c(0, 1),
+  ylim = c(
+    0.5,
+    length(phylo_tree$tip.label) + 0.5),
+  xaxt = "n",
+  yaxt = "n",
+  xlab = "",
+  ylab = "",
+  bty = "n")
+
+# add abbreviated species names
+text(
+  x = 0,
+  y = tip_y,
+  labels = phylo_tree_plot$tip.label,
+  pos = 4,
+  offset = 0,
+  font = 3,
+  cex = 0.6)
+
+##### variance partitioning #####
+par(
+  fig = c(0.17, 1.00, main_bottom, main_top),
+  mar = c(2.2, 0, 0, 0),
+  mgp = c(1.7, 0.5, 0),
+  xpd = FALSE,
+  new = TRUE)
+
+# set up plotting region
+plot(
+  NA,
+  xlim = c(
+    0,
+    max(colSums(vp_grouped_vals_phylo)) * 1.02),
+  ylim = c(
+    0.5,
+    length(phylo_tree$tip.label) + 0.5),
+  xaxt = "n",
+  yaxt = "n",
+  xlab = "",
+  ylab = "",
+  bty = "n")
+
+# height of each horizontal bar
+bar_height = 1
+
+# draw stacked variance partitioning bars
+for (i in seq_len(ncol(vp_grouped_vals_phylo))) {
+  
+  # NEW: plot a single grey bar for species with poor beta convergence
+  if (problem_idx[i]) {
+    
+    rect(
+      xleft = 0,
+      ybottom = tip_y[i] - bar_height / 2,
+      xright = sum(vp_grouped_vals_phylo[, i]),
+      ytop = tip_y[i] + bar_height / 2,
+      col = poor_convergence_col,
+      border = "black"
+    )
+    
+  } else {
+    
+    # plot the usual stacked variance partition for all other species
+    x_left = 0
+    
+    for (j in seq_len(nrow(vp_grouped_vals_phylo))) {
+      
+      x_right = x_left + vp_grouped_vals_phylo[j, i]
+      
+      rect(
+        xleft = x_left,
+        ybottom = tip_y[i] - bar_height / 2,
+        xright = x_right,
+        ytop = tip_y[i] + bar_height / 2,
+        col = vp_cols[j],
+        border = "black"
+      )
+      
+      x_left = x_right
+    }
+  }
+}
+
+# add x-axis
+axis(
+  side = 1,
+  cex.axis = 0.9,
+  tck = -0.02)
+
+# add x-axis title
+mtext(
+  "Proportion of variance in occurrence",
+  side = 1,
+  line = -1.75,
+  cex = 0.9)
+
+##### legend #####
+par(
+  fig = c(0.00, 1.00, 0.00, main_bottom),
+  mar = c(0, 0, 0, 0),
+  xpd = FALSE,
+  new = TRUE)
+
+# create empty plotting region for legend
+plot.new()
+
+# add poor-convergence category to legend
+legend(
+  "top",
+  legend = c(
+    legend_labels_phylo,
+    "Poor β convergence – not interpreted"
+  ),
+  fill = c(
+    vp_cols,
+    "grey40"
+  ),
+  ncol = 3,
+  bty = "n",
+  cex = 0.9,
+  x.intersp = 0.5,
+  y.intersp = 0.9)
+
+dev.off()
+
+#### UNGROUPED - PHYLOGENETIC ORDERING ####
+# extract the phylogenetic tree directly from the Hmsc model
+phylo_tree = PA_model$phyloTree
+
+# create a copy of the tree for plotting
+phylo_tree_plot = phylo_tree
+
+# create abbreviated species labels for plotting
+phylo_tree_plot$tip.label = sapply(
+  strsplit(phylo_tree$tip.label, "\\."),
+  function(x) paste0(
+    substr(x[1], 1, 1),
+    ". ",
+    paste(x[-1], collapse = " ")))
+
+# extract the phylogenetic tip order
+phylo_order = match(
+  phylo_tree$tip.label,
+  colnames(vp_ungrouped_vals))
+
+# check that all species are present in the variance partitioning results
+if (any(is.na(phylo_order))) {
+  stop("some species in the phylogenetic tree are missing from vp_ungrouped_vals.")
+}
+
+# reorder variance partitioning values according to phylogenetic tip order
+vp_ungrouped_vals_phylo = vp_ungrouped_vals[, phylo_order]
+
+# calculate mean variance explained by each component
+vp_ungrouped_means_phylo = rowMeans(vp_ungrouped_vals_phylo)
+
+# create legend labels with means
+legend_labels_phylo = paste0(
+  rownames(vp_ungrouped_vals_phylo),
+  " (mean = ",
+  round(vp_ungrouped_means_phylo, 2),
+  ")")
+
+##### identify species with poor beta convergence #####
+# species for which variance partitioning should not be interpreted
+problem_species = c(
+  "Neocirrhites.armatus",
+  "Zebrasoma.scopas",
+  "Centropyge.flavissima")
+
+# identify their positions in the phylogenetically ordered results
+problem_idx = phylo_tree$tip.label %in% problem_species
+
+# check that all specified species occur in the tree
+if (!all(problem_species %in% phylo_tree$tip.label)) {
+  stop("one or more problem_species are missing from the phylogenetic tree.")
+}
+
+##### determine how much of the phylogeny to show #####
+# calculate the depth of each node from the root
+node_depth = ape::node.depth.edgelength(phylo_tree)
+
+# find the maximum root-to-tip distance
+max_tree_depth = max(node_depth)
+
+# proportion of the tree to show
+recent_tree_fraction = 0.10
+
+# calculate the left-hand limit of the displayed tree
+tree_xmin = max_tree_depth - (
+  max_tree_depth * recent_tree_fraction)
+
+##### create portrait figure #####
+jpeg(
+  filename = here(
+    "Figures","PA_Model",
+    "Variance_Partitioning_Ungrouped_Portrait_Phylogenetic_Recent.jpg"),
+  width = 11,
+  height = 14,
+  units = "in",
+  res = 600,
+  quality = 100)
+
+##### colors #####
+vp_cols = colorRamps::matlab.like(
+  nrow(vp_ungrouped_vals_phylo))
+
+# color used to flag species with poor beta convergence
+poor_convergence_col = "grey40"
+
+##### main plot area #####
+# tree, labels and variance plot use exactly the same vertical coordinates
+main_bottom = 0.07
+main_top = 1
+
+##### phylogenetic tree #####
+par(
+  fig = c(0.00, 0.1, main_bottom, main_top),
+  mar = c(2.2, 0, 0, 0),
+  xpd = FALSE)
+
+# plot the terminal portion of the phylogeny without tip labels
+ape::plot.phylo(
+  phylo_tree_plot,
+  type = "phylogram",
+  direction = "rightwards",
+  show.tip.label = FALSE,
+  edge.width = 0.75,
+  x.lim = c(
+    tree_xmin,
+    max_tree_depth),
+  y.lim = c(
+    0.5,
+    length(phylo_tree$tip.label) + 0.5),
+  no.margin = FALSE)
+
+# retrieve the coordinates used by plot.phylo
+phylo_coords = get(
+  "last_plot.phylo",
+  envir = .PlotPhyloEnv)
+
+# extract the y-position of each species tip
+tip_y = phylo_coords$yy[
+  seq_len(length(phylo_tree$tip.label))]
+
+##### species labels #####
+par(
+  fig = c(0.1, 0.21, main_bottom, main_top),
+  mar = c(2.2, 0, 0, 0),
+  xpd = FALSE,
+  new = TRUE)
+
+# create empty plotting region for species labels
+plot(
+  NA,
+  xlim = c(0, 1),
+  ylim = c(
+    0.5,
+    length(phylo_tree$tip.label) + 0.5),
+  xaxt = "n",
+  yaxt = "n",
+  xlab = "",
+  ylab = "",
+  bty = "n")
+
+# add abbreviated species names
+text(
+  x = 0,
+  y = tip_y,
+  labels = phylo_tree_plot$tip.label,
+  pos = 4,
+  offset = 0,
+  font = 3,
+  cex = 0.6)
+
+##### variance partitioning #####
+par(
+  fig = c(0.17, 1.00, main_bottom, main_top),
+  mar = c(2.2, 0, 0, 0),
+  mgp = c(1.7, 0.5, 0),
+  xpd = FALSE,
+  new = TRUE)
+
+# set up plotting region
+plot(
+  NA,
+  xlim = c(
+    0,
+    max(colSums(vp_ungrouped_vals_phylo)) * 1.02),
+  ylim = c(
+    0.5,
+    length(phylo_tree$tip.label) + 0.5),
+  xaxt = "n",
+  yaxt = "n",
+  xlab = "",
+  ylab = "",
+  bty = "n")
+
+# height of each horizontal bar
+bar_height = 1
+
+# draw stacked variance partitioning bars
+for (i in seq_len(ncol(vp_ungrouped_vals_phylo))) {
+  
+  # plot a single grey bar for species with poor beta convergence
+  if (problem_idx[i]) {
+    
+    rect(
+      xleft = 0,
+      ybottom = tip_y[i] - bar_height / 2,
+      xright = sum(vp_ungrouped_vals_phylo[, i]),
+      ytop = tip_y[i] + bar_height / 2,
+      col = poor_convergence_col,
+      border = "black"
+    )
+    
+  } else {
+    
+    # plot the usual stacked variance partition for all other species
+    x_left = 0
+    
+    for (j in seq_len(nrow(vp_ungrouped_vals_phylo))) {
+      
+      x_right = x_left + vp_ungrouped_vals_phylo[j, i]
+      
+      rect(
+        xleft = x_left,
+        ybottom = tip_y[i] - bar_height / 2,
+        xright = x_right,
+        ytop = tip_y[i] + bar_height / 2,
+        col = vp_cols[j],
+        border = "black"
+      )
+      
+      x_left = x_right
+    }
+  }
+}
+
+# add x-axis
+axis(
+  side = 1,
+  cex.axis = 0.8,
+  tck = -0.02)
+
+# add x-axis title
+mtext(
+  "Proportion of variance in occurrence",
+  side = 1,
+  line = -1.75,
+  cex = 0.8)
+
+##### legend #####
+par(
+  fig = c(0.00, 1.00, 0.00, main_bottom),
+  mar = c(0, 0, 0, 0),
+  xpd = FALSE,
+  new = TRUE)
+
+# create empty plotting region for legend
+plot.new()
+
+# add poor-convergence category to legend
+legend(
+  "top",
+  legend = c(
+    legend_labels_phylo,
+    "Poor β convergence – not interpreted"
+  ),
+  fill = c(
+    vp_cols,
+    "grey40"
+  ),
+  ncol = 3,
+  bty = "n",
+  cex = 0.8,
+  x.intersp = 0.5,
+  y.intersp = 0.9)
+
+dev.off()
